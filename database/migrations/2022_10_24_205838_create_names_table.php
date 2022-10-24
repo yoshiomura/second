0d421b4201a1a43b22a4aa5d9d81875f2aa61c21
yoshiomura <yoshi440011@docomo.ne.jp>
2022-10-24 21:17:34 +0900

    second

--- database/migrations/2022_10_24_205838_create_names_table.php
+++ database/migrations/2022_10_24_205838_create_names_table.php
@@ -13,13 +13,10 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('names', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('exercise');
+            $table->foreignId('training_id')->constrained();
             $table->timestamps();
         });
     }
@@ -31,6 +28,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('names');
     }
 };
